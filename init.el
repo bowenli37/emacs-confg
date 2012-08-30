0c86b87fd19660e222c0088f0604db53a96ed94a
kotfic <kotfic@gmail.com>
2012-08-29 21:23:47 -0400

    adding orgmode 7.9

--- init.el
+++ init.el
@@ -10,6 +10,21 @@
 
 
 ;;;;
+;;;;    Org-Mode Configs
+;;;;
+
+(add-to-list 'load-path "~/.emacs.d/lib/org/")
+(add-to-list 'load-path "~/.emacs.d/lib/org/contrib/lisp/")
+(require 'org)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;;;
 ;;;;   IDO config
 ;;;;
 
