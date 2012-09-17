72cd7d2fa8343334893a0d5098c1fa2b8a088b1d
kotfic <kotfic@gmail.com>
2012-09-17 14:48:58 -0400

    changing up modeline a little

--- init.el
+++ init.el
@@ -9,6 +9,19 @@
 (column-number-mode t)
 
 
+
+;;;;
+;;;;    Modeline
+;;;;
+
+(set-face-background 'mode-line nil)
+(set-face-background 'mode-line-inactive nil)
+
+
+(set-face-foreground 'mode-line "#ffff87")
+(set-face-foreground 'mode-line-inactive "#ffff87")
+
+
 ;;;;
 ;;;;    Org-Mode Configs
 ;;;;
@@ -155,23 +168,26 @@
 ;;;;
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#1d1f21"))
  '(custom-enabled-themes nil)
  '(custom-safe-themes (quote ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(fci-rule-color "#282a2e")
  '(geben-dbgp-feature-list (quote ((:set max_data 32768) (:set max_depth 2) (:set max_children 32) (:get breakpoint_types geben-dbgp-breakpoint-store-types))))
+ '(org-agenda-files (quote ("~/classes/IIST669/notes.org" "~/classes/IIST617/notes.org")))
+ '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf\\'" . "/usr/bin/evince"))))
  '(py-shell-name "ipython"))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
 
 
+(put 'scroll-left 'disabled nil)
