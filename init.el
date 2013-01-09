1e39b5613ca28533b69572e159259a7d81942ded
Christopher Kotifla <kotfic@gmail.com>
2013-01-09 11:48:44 -0500

    adding burst coding-standards submodule,  adding different php-mode as submodule

--- init.el
+++ init.el
@@ -338,43 +338,11 @@ header."
 (autoload 'php-mode "php-mode" "Major mode for PHP." t)
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|phtml\\)\\'" . php-mode))
 
-(defun buffer-cleanup()
-  (interactive)
-  (buffer-cleanup-safe)
-  (indent-region (point-min) (point-max)))
+;;;;
+;;;;   Burst Marketing - Coding standards
+;;;;
 
-(defun buffer-cleanup-safe()
-  "Unless its a markdown file, do some cleaning up."
-  (interactive)
-  (unless (and (buffer-file-name)
-               (string-equal (file-name-extension (buffer-file-name)) "md"))
-    (whitespace-cleanup)
-    (untabify (point-min) (point-max))
-    (set-buffer-file-coding-system 'utf-8)))
-
-(global-set-key (kbd "C-c n") 'buffer-cleanup)
-
-; (add-hook 'before-save-hook 'buffer-cleanup-safe) ;; DUBIOUS!
-
-
-(add-hook 'php-mode-hook (lambda()
-                           (setq indent-tabs-mode nil)
-                           (setq tab-width 2)
-                           (setq c-basic-offset 2)))
-
-(require 'flymake)
-(add-hook 'php-mode-hook 'flymake-mode-on)
-
-(require 'align)
-(add-to-list 'align-rules-list
-             `(php-array-keys
-               (regexp  . "\\(\\s-*\\)=")
-               (justify . nil)
-               (repeat  . nil)
-               (modes   . '(php-mode))
-               (tab-stop)))
-
-
+(load "~/.emacs.d/lib/burst/coding-standards")
 
 ;;;;
 ;;;;   LISP/SLIME Support
