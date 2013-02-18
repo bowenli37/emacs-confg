e8793d0dfeb30173b5e52fc1fff7b5c6562dc476
Christopher Kotfila <kotfic@gmail.com>
2013-02-18 09:06:17 -0500

    deleting/ignoring org-clock-save.el and changing browser support to conkeror

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - carasgaladon at <2013-02-08 Fri 12:51>
-(setq stored-clock-history '(("/home/kotfic/org/school/classes/IIST560/notes.org" . 14037) ("/home/kotfic/org/school/OSF2013/notes.org" . 15915) ("/home/kotfic/org/personal/wedding.org" . 8320) ("/home/kotfic/org/work/time.org" . 19) ("/home/kotfic/org/work/time.org" . 428)))
