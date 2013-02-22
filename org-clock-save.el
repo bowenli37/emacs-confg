900b6ae15e3d93c87c6b0acf10e9d2ab8494b880
Christopher Kotfila <kotfic@gmail.com>
2013-02-22 03:47:52 -0500

    org-clock - shouldn't we be ignoring this?

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - carasgaladon at <2013-02-08 Fri 12:51>
-(setq stored-clock-history '(("/home/kotfic/org/school/classes/IIST560/notes.org" . 14037) ("/home/kotfic/org/school/OSF2013/notes.org" . 15915) ("/home/kotfic/org/personal/wedding.org" . 8320) ("/home/kotfic/org/work/time.org" . 19) ("/home/kotfic/org/work/time.org" . 428)))
+;; org-persist.el - carasgaladon at <2013-02-12 Tue 14:20>
+(setq stored-clock-history '(("/home/kotfic/org/work/time.org" . 953) ("/home/kotfic/org/work/time.org" . 1047) ("/home/kotfic/org/school/OSF2013/notes.org" . 16019) ("/home/kotfic/org/unfiled.org" . 989) ("/home/kotfic/org/school/classes/IIST560/notes.org" . 14537)))
