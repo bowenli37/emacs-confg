77cb8c62e9567a0762c8637e58787e22cb6bd6ae
Christopher Kotfila <kotfic@gmail.com>
2013-02-25 03:22:48 -0500

    adding auctex and reftex support

--- init.el
+++ init.el
@@ -14,6 +14,35 @@
 (setq indent-tabs-mode nil)
 
 
+;;;;
+;;;;   Lorem Ipsum
+;;;;
+(add-to-list 'load-path "~/.emacs.d/lib/lorem-ipsum")
+(require 'lorem-ipsum)
+
+
+;;;;
+;;;;   AucTex
+;;;;
+
+(add-to-list 'load-path "~/.emacs.d/lib/")
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;;;;
+;;;;    RefTex
+;;;;
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
+(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
+
+
+
+(setq reftex-plug-into-AUCTeX t)
+
 
 ;;;;
 ;;;;   Tidy Integration
