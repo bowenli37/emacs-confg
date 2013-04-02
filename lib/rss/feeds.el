6f814698b011946be4ce0b4999e618e2ffdb56a3
Christopher Kotfila <kotfic@gmail.com>
2013-04-02 11:36:21 -0400

    adding feeds definition

---
+++ lib/rss/feeds.el
@@ -0,0 +1,134 @@
+(setq org-feed-alist
+      '(("ACRL Insider"
+         "http://feeds.feedburner.com/acrlinsider"
+         "~/org/feeds.org"
+         "ACRL Insider")
+        ("ACRL Preprints"
+         "http://feeds2.feedburner.com/crlpreprints"
+         "~/org/feeds.org"
+         "ACRL Preprints")
+        ("ACRLog"
+         "http://acrlog.org/feed/"
+         "~/org/feeds.org"
+         "ACRLog")
+        ("Association of Research Libraries (ARL) - Full Feed"
+         "http://www.arl.org/bm~feed.xml"
+         "~/org/feeds.org"
+         "ARL - Full Feed")
+        ("CNI: Coalition for Networked Information"
+         "http://www.cni.org/feed/"
+         "~/org/feeds.org"
+         "CNI: Coalition for Networked Information")
+        ("CNI: Coalition for Networked Information» CNI Conversations"
+         "http://www.cni.org/category/cni-conversations-2/feed/"
+         "~/org/feeds.org"
+         "CNI: Coalition for Networked Information» CNI Conversations")
+        ("code4lib - coders for libraries, libraries for coders"
+         "http://code4lib.org/node/feed"
+         "~/org/feeds.org"
+         "code4lib - coders for libraries, libraries for coders")
+        ("College &amp; Research Libraries News current issue"
+         "http://crln.acrl.org/rss/current.xml"
+         "~/org/feeds.org"
+         "College &amp; Research Libraries News current issue")
+        ("College &amp; Research Libraries News recent issues"
+         "http://crln.acrl.org/rss/recent.xml"
+         "~/org/feeds.org"
+         "College &amp; Research Libraries News recent issues")
+        ("EDUCAUSE | Articles"
+         "http://www.educause.edu/browse/content/all/44823/feed"
+         "~/org/feeds.org"
+         "EDUCAUSE | Articles")
+        ("EDUCAUSE | Blog"
+         "http://www.educause.edu/browse/content/all/44836/feed"
+         "~/org/feeds.org"
+         "EDUCAUSE | Blog")
+        ("EDUCAUSE | Departments"
+         "http://www.educause.edu/browse/content/all/44828/feed"
+         "~/org/feeds.org"
+         "EDUCAUSE | Departments")
+        ("In the Library with the Lead Pipe"
+         "http://www.inthelibrarywiththeleadpipe.org/feed/"
+         "~/org/feeds.org"
+         "In the Library with the Lead Pipe")
+        ("Information Technology and Libraries"
+         "http://ejournals.bc.edu/ojs/index.php/ital/gateway/plugin/WebFeedGatewayPlugin/rss2"
+         "~/org/feeds.org"
+         "Information Technology and Libraries")
+        ("Inside Higher Ed | Blog U"
+         "http://www.insidehighered.com/blogs/feed/all"
+         "~/org/feeds.org"
+         "Inside Higher Ed | Blog U")
+        ("Inside Higher Ed | News"
+         "http://www.insidehighered.com/news/feed"
+         "~/org/feeds.org"
+         "Inside Higher Ed | News")
+        ("Journal of Library Administration"
+         "http://www.tandfonline.com/action/showFeed?ui=0&amp;mi=5tasfv&amp;ai=1d0&amp;jc=wjla20&amp;type=etoc&amp;feed=rss"
+         "~/org/feeds.org"
+         "Journal of Library Administration")
+        ("LIBLICENSE-L List"
+         "http://listserv.crl.edu/wa.exe?RSS&amp;L=LIBLICENSE-L&amp;v=1.0"
+         "~/org/feeds.org"
+         "LIBLICENSE-L List")
+        ("OATP primary"
+         "http://tagteam.harvard.edu/remix/oatp/items.rss"
+         "~/org/feeds.org"
+         "OATP primary")
+        ("ProfHacker"
+         "http://chronicle.com/blogs/profhacker/feed"
+         "~/org/feeds.org"
+         "ProfHacker")
+        ("SPARC - Full Feed"
+         "http://www.arl.org/sparc/bm~feed.xml"
+         "~/org/feeds.org"
+         "SPARC - Full Feed")
+        ("The Journal of Academic Librarianship"
+         "http://rss.sciencedirect.com/publication/science/00991333"
+         "~/org/feeds.org"
+         "The Journal of Academic Librarianship")
+        ("The Ubiquitous Librarian"
+         "http://chronicle.com/blognetwork/theubiquitouslibrarian/feed/"
+         "~/org/feeds.org"
+         "The Ubiquitous Librarian")
+        ("Deathray Research"
+         "http://deathrayresearch.tumblr.com/rss"
+         "~/org/feeds.org"
+         "Deathray Research")
+        ("emacs-fu"
+         "http://emacs-fu.blogspot.com/feeds/posts/default"
+         "~/org/feeds.org"
+         "emacs-fu")
+        ("High Scalability"
+         "http://feeds.feedburner.com/HighScalability"
+         "~/org/feeds.org"
+         "High Scalability")
+        ("M-x emacs-reddit"
+         "http://www.reddit.com/r/emacs/.rss"
+         "~/org/feeds.org"
+         "M-x emacs-reddit")
+        ("Martin Fowler"
+         "http://martinfowler.com/feed.atom"
+         "~/org/feeds.org"
+         "Martin Fowler")
+        ("Mastering Emacs"
+         "http://www.masteringemacs.org/feed/"
+         "~/org/feeds.org"
+         "Mastering Emacs")
+        ("OWASP Moderated AppSec News Feed's shared items"
+         "http://www.google.com/reader/public/atom/user/16712724397688793161/state/com.google/broadcast"
+         "~/org/feeds.org"
+         "OWASP Moderated AppSec News Feed's shared items")
+        ("ReadWriteWeb"
+         "http://feeds.feedburner.com/readwriteweb"
+         "~/org/feeds.org"
+         "ReadWriteWeb")
+        ("Software Best Practices"
+         "http://forums.construx.com/blogs/MainFeed.aspx?GroupID=5&amp;Type=AllBlogs"
+         "~/org/feeds.org"
+         "Software Best Practices")
+        ("UXmatters"
+         "http://uxmatters.com/index.xml"
+         "~/org/feeds.org"
+         "UXmatters")
+        ))
